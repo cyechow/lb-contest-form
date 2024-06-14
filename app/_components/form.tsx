956b8451aae28201bb860de68c71517ddb230581
Leeyanhawrt <leeyan1108@gmail.com>
2024-06-14 16:42:12 -0400

    Make text changes and slight styling

--- app/_components/form.tsx
+++ app/_components/form.tsx
@@ -1,104 +1,105 @@
-import { useFormik, Form, FormikProvider, useField } from "formik"
-import { saveEntry } from "@/app/_lib/actions"
-import * as Yup from "yup"
+import { useFormik, Form, FormikProvider, useField } from "formik";
+import { saveEntry } from "@/app/_lib/actions";
+import * as Yup from "yup";
 import React from "react";
 
 interface formProps {
-	setSubmitted: () => void
+  setSubmitted: () => void;
 }
 
 interface inputProps {
-	label: string,
-	helpText: string,
-	id: string,
-	name: string,
-	type: string
+  label: string;
+  helpText: string;
+  id: string;
+  name: string;
+  type: string;
 }
 
-const FormInput = ({ label, helpText, id, name, type } : inputProps) => {
-	const [field, meta] = useField({ id, name, type })
+const FormInput = ({ label, helpText, id, name, type }: inputProps) => {
+  const [field, meta] = useField({ id, name, type });
 
-	const [didFocus, setDidFocus] = React.useState(false);
-	const handleFocus = () => setDidFocus(true);
-	const showFeedback = (!!didFocus && field.value.trim().length > 2) || meta.touched;
+  const [didFocus, setDidFocus] = React.useState(false);
+  const handleFocus = () => setDidFocus(true);
+  const showFeedback =
+    (!!didFocus && field.value.trim().length > 2) || meta.touched;
 
-	return (
-		<div className={`form-control ${
-			showFeedback ? (meta.error ? 'invalid' : 'valid') : ''
-		}`}>
-			<div className="flex items-center space-between">
-				<label
-					htmlFor={id}
-				>{label}</label>{' '}
-				{showFeedback ? (
-					<div
-						id={`${id}-feedback`}
-						aria-live="polite"
-						className="feedback text-sm"
-					>
-						{meta.error ? meta.error: '✓'}
-					</div>
-				) : null}
-			</div>
-			<div className="flex items-center space-between">
-				<input
-					{...{id, name, type}}
-					{...field}
-					className="flex-1 border-2 border-gray-500 p-2 rounded-md focus:border-red-500 focus:ring-red-500"
-					aria-describedby={`${id}-feedback ${id}-help`}
-					onFocus={handleFocus}
-				/>
-			</div>
-			<div className="text-xs" id={`${id}-help`} tabIndex={-1}>
-				{helpText}
-			</div>
-		</div>
-	)
-}
+  return (
+    <div
+      className={`form-control ${
+        showFeedback ? (meta.error ? "invalid" : "valid") : ""
+      }`}
+    >
+      <div className="flex items-center space-between">
+        <label htmlFor={id}>{label}</label>{" "}
+        {showFeedback ? (
+          <div
+            id={`${id}-feedback`}
+            aria-live="polite"
+            className="feedback text-sm"
+          >
+            {meta.error ? meta.error : "✓"}
+          </div>
+        ) : null}
+      </div>
+      <div className="flex items-center space-between">
+        <input
+          {...{ id, name, type }}
+          {...field}
+          className="flex-1 border-2 border-gray-500 p-2 rounded-md focus:border-red-500 focus:ring-red-500"
+          aria-describedby={`${id}-feedback ${id}-help`}
+          onFocus={handleFocus}
+        />
+      </div>
+      <div className="text-xs" id={`${id}-help`} tabIndex={-1}>
+        {helpText}
+      </div>
+    </div>
+  );
+};
 
-export default function EntryForm({setSubmitted} : formProps) {
+export default function EntryForm({ setSubmitted }: formProps) {
+  // Formik - to handle all form-related things like validation and submission
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      ighandle: "",
+    },
 
-	// Formik - to handle all form-related things like validation and submission
-	const formik = useFormik({
-		initialValues: {
-			name: '',
-			ighandle: ''
-		},
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(2, "Name must be at least 2 characters long")
+        .required("Name is required"),
+      ighandle: Yup.string()
+        .matches(RegExp("^[a-zA-Z0-9.]+$"), "Valid Instagram handle required.")
+        .max(30, "Valid Instagram handles are 30 characters or less.")
+        .required("Instagram handle is required."),
+    }),
 
-		validationSchema: Yup.object({
-		name: Yup.string()
-			.min(2, 'Name must be at least 2 characters long')
-			.required('Name is required'),
-		ighandle: Yup.string().matches(RegExp("^[a-zA-Z0-9.]+$"), "Valid Instagram handle required.").max(30, "Valid Instagram handles are 30 characters or less.")
-			.required('Instagram handle is required.')
-		}),
-
-		onSubmit: async (values) => {
-			saveEntry(values)
-			setSubmitted()
-		}
-	});
-	return (
-		<FormikProvider value={formik} >
-			<Form>
-				<FormInput
-					label="Name"
-					id="name"
-					name="name"
-					helpText=""
-					type="text"
-				/>
-				<FormInput
-					label="Instagram Handle"
-					id="ighandle"
-					name="ighandle"
-					helpText="Must be a valid IG handle, 30 characters or less and cannot contain any special characters aside from '.'"
-					type="text"
-				/>
-				<div>
-					<button type="submit" className="bg-gray-500 font-bold text-sm text-white py-3 mt-6 rounded-lg w-full">Submit</button>
-				</div>
-			</Form>
-		</FormikProvider>
-	)
+    onSubmit: async (values) => {
+      saveEntry(values);
+      setSubmitted();
+    },
+  });
+  return (
+    <FormikProvider value={formik}>
+      <Form>
+        <FormInput label="Name" id="name" name="name" helpText="" type="text" />
+        <FormInput
+          label="Instagram Handle/Name"
+          id="ighandle"
+          name="ighandle"
+          type="text"
+          helpText=""
+        />
+        <div>
+          <button
+            type="submit"
+            className="bg-purple-500 font-bold text-sm text-white py-3 mt-6 rounded-lg w-full hover:bg-pink-400 transition"
+          >
+            Submit
+          </button>
+        </div>
+      </Form>
+    </FormikProvider>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
@@ -6,16 +6,14 @@ import ThankYou from "./_components/thankyou";
 import EntryForm from "./_components/form";
 
 export default function Home() {
-  const [submitted, setSubmitted] = useState(false)
+  const [submitted, setSubmitted] = useState(false);
 
   const resetForm = () => {
     setSubmitted(false);
-  }
+  };
 
   if (submitted) {
-    return (
-      <ThankYou resetSubmitted={ resetForm } />
-    )
+    return <ThankYou resetSubmitted={resetForm} />;
   }
 
   return (
@@ -23,14 +21,44 @@ export default function Home() {
       <main className="flex flex-row min-h-screen justify-center items-center bg-white font-sans">
         <div className="flex">
           <div className="flex-1 text-gray-700 p-20">
-              <h1 className="text-3xl font-bold pb-6">LimitBreaker Giveaway Contest</h1>
-              <p className="pb-4">Enter our giveaway contest for a chance to win limited LimitBreaker items!</p>
-              <p className="pb-4">To enter the draw, please follow LimitBreaker on Instagram <a href="https://www.instagram.com/lb.limitbreaker/" className="text-blue-500 underline">@lb.limitbreaker</a>
-                {' '}and submit your name and IG handle.</p>
-              <p className="pb-4">The winners will be announce on <b className="text-lg text-red-500">June 26th, 2024</b></p>
-              <div className="mt-6">
-                <EntryForm setSubmitted={() => setSubmitted(true)} />
-              </div>
+            <h1 className="text-3xl font-bold mb-6">
+              LimitBreaker Giveaway Contest
+            </h1>
+            <p className="mb-4">
+              🎉 Join Our Giveaway Contest for a Chance to Win Exclusive
+              LimitBreaker Items! 🎉
+            </p>
+            <p>To enter:</p>
+            <ol className="my-4 ml-4 flex-col flex gap-1">
+              <li className="flex gap-2">
+                <p>1.</p>
+                <p>
+                  Follow us on Instagram{" "}
+                  <a
+                    href="https://www.instagram.com/lb.limitbreaker/"
+                    className="text-blue-500 underline"
+                  >
+                    @lb.limitbreaker
+                  </a>
+                </p>
+              </li>
+              <li className="flex gap-1">
+                <p>2.</p>
+                <p>
+                  Submit your name and Instagram handle using the form below
+                </p>
+              </li>
+            </ol>
+            <p className="mb-4">
+              Winners will be announced on our Instagram page on{" "}
+              <span className="text-lg text-red-500 font-bold">
+                June 26th, 2024{" "}
+              </span>{" "}
+              and contacted via direct message.
+            </p>
+            <div className="mt-6">
+              <EntryForm setSubmitted={() => setSubmitted(true)} />
+            </div>
           </div>
         </div>
       </main>
