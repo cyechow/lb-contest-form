48828b951638c2be02d8a67ba34128440e659396
Edith <edith.cy.chow@gmail.com>
2024-06-18 22:11:06 -0400

    font type changes & text mods.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useState } from "react";
+import { Bebas_Neue } from "next/font/google";
 
 import ThankYou from "./_components/thankyou";
 import EntryForm from "./_components/form";
+
+const bebas_neue = Bebas_Neue({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Home() {
   const [submitted, setSubmitted] = useState(false);
@@ -18,14 +25,14 @@ export default function Home() {
 
   return (
     <div>
-      <main className="flex flex-row min-h-screen justify-center items-center bg-white font-sans">
+      <main className="flex flex-row min-h-screen justify-center items-center bg-white">
         <div className="flex">
           <div className="flex-1 text-gray-700 p-20">
-            <h1 className="text-3xl font-bold mb-6">
-              LimitBreaker Giveaway Contest
+            <h1 className={`text-3xl font-bold mb-6 ${bebas_neue.className}`}>
+              LimitBreaker Giveaway
             </h1>
             <p className="mb-4">
-              🎉 Join Our Giveaway Contest for a Chance to Win Exclusive
+              🎉 Join Our Giveaway for a Chance to Win Exclusive
               LimitBreaker Items! 🎉
             </p>
             <p>To enter:</p>
@@ -42,7 +49,7 @@ export default function Home() {
                   </a>
                 </p>
               </li>
-              <li className="flex gap-1">
+              <li className="flex gap-2">
                 <p>2.</p>
                 <p>
                   Submit your name and Instagram handle using the form below
@@ -52,7 +59,7 @@ export default function Home() {
             <p className="mb-4">
               Winners will be announced on our Instagram page on{" "}
               <span className="text-lg text-red-500 font-bold">
-                June 26th, 2024{" "}
+                June 23rd, 2024{" "}
               </span>{" "}
               and contacted via direct message.
             </p>
